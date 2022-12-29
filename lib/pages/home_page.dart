deb4a3f122369a6fdcfa006f6155e76ba5332868
fen0268 <Kenshi150107@gmail.com>
2022-12-30 01:10:45 +0900

    appbarを削除

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,31 +3,16 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import '../widgets/floating_action_button_widget.dart';
 import '../widgets/sf_calendar/syncfusion_calendar_widget.dart';
-import '../widgets/table_calendar_widget.dart';
 
 class HomePage extends ConsumerWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-        appBar: AppBar(
-          actions: [
-            IconButton(
-              onPressed: () {
-                Navigator.of(context).push<void>(
-                  MaterialPageRoute(
-                    builder: (context) => const TableCalendarWidget(),
-                  ),
-                );
-              },
-              icon: const Icon(Icons.abc),
-            ),
-          ],
-        ),
-        body: const SyncfusionCalendarWidget(),
-        floatingActionButton: const FloatingActionButtonWidget(),
+        body: SyncfusionCalendarWidget(),
+        floatingActionButton: FloatingActionButtonWidget(),
       ),
     );
   }
