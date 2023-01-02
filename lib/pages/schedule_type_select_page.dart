a4797a5c28793d3a3e48f82733b6451693ed7fb9
fen0268 <Kenshi150107@gmail.com>
2023-01-03 05:13:01 +0900

    詳細カスタムページを

--- lib/pages/schedule_type_select_page.dart
+++ lib/pages/schedule_type_select_page.dart
@@ -6,6 +6,36 @@ class ScheduleTypeSelectPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return Container();
+    return Scaffold(
+      body: Container(
+        padding: const EdgeInsets.only(top: 10, left: 35),
+        child: ListView(
+          children: const [
+            ListTile(
+              leading: Icon(Icons.arrow_back),
+              title: Text('繰り返し'),
+            ),
+            ListTile(
+              title: Text('繰り返さない'),
+            ),
+            ListTile(
+              title: Text('毎日'),
+            ),
+            ListTile(
+              title: Text('毎週'),
+            ),
+            ListTile(
+              title: Text('毎月'),
+            ),
+            ListTile(
+              title: Text('毎年'),
+            ),
+            ListTile(
+              title: Text('カスタム'),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
